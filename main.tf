46b577d5d031bfe28094d1b5dc7a5117ca1fcf44
unknown <imad3290@gmail.com>
2025-07-12 18:01:18 +0530

    initial transfer of file

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.34.0"
+    }
+  }
+}
+
+provider "azurerm" {
+ features {}
+ subscription_id = "913de002-f9f5-45e9-bd3b-7bc25c251aff"
+}
+
+resource "azurerm_resource_group" "testing_rg" {
+  name = var.resource_rgs
+  location = var.resource_location
+  
+}
---
+++ variable.tf
@@ -0,0 +1,6 @@
+variable "resource_rgs" {
+  type = string
+}
+variable "resource_location" {
+  type = string
+}
